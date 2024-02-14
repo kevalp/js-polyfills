319ec002450d61e541f1aca6cac80e4e1f5e78ee
Keval <kevalp@users.noreply.github.com>
2024-02-14 13:54:00 +0530

    Update array.js add 'concat' function

--- array.js
+++ array.js
@@ -9,6 +9,23 @@ function at(targetArray, index) {
 }
 
 function concat(targetArray) {
+	if (!targetArray || !Array.isArray(targetArray)) {
+		throw new Error('First argument needs to be an array');
+	}
+	const result = [...targetArray];	
+	for (let i=1; i<arguments.length; i++) {
+		const item = arguments[i];
+		if (typeof item[Symbol.iterator] === 'function' && typeof item !== 'string') {
+			console.log(item, 'is iterable');
+			for (let subitem of item) {
+				result.push(subitem);
+			}
+		} else {
+			result.push(item);
+		}
+	}
+	return result;
 }
+
 
 export { at, concat };
