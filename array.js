a9a6502f13826f2d51efde7d454d224040161b7c
Keval <kevalp@users.noreply.github.com>
2024-02-14 09:02:51 +0530

    Add 'concat' method

--- array.js
+++ array.js
@@ -1,11 +1,14 @@
-function at(array, index) {
-	if (!array || !Array.isArray(array)) {
+function at(targetArray, index) {
+	if (!targetArray || !Array.isArray(targetArray)) {
 		throw new Error('First argument needs to be an Array')
 	}
 	if (!parseInt(index)) {
 		throw new Error('Second argument needs to be an integer');
 	}
-	return array[(index >= 0) ? parseInt(index) : parseInt(array.length + index)]
+	return targetArray[(index >= 0) ? parseInt(index) : parseInt(targetArray.length + index)]
 }
 
-export { at };
+function concat(targetArray) {
+}
+
+export { at, concat };
