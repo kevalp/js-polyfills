cfbee98fba2088e23383126ecaf2ee17d33cf997
Keval <kevalp@users.noreply.github.com>
2024-02-16 10:59:47 +0530

    Add 'every' method

--- array.js
+++ array.js
@@ -38,5 +38,19 @@ function entries(targetArray) {
 	return result;
 }
 
+function every(targetArray, fn) {
+	if (!targetArray || !Array.isArray(targetArray)) {
+		throw new Error('First argument needs to be an Array');
+	}
+	if (!fn || typeof fn !== 'function') {
+		throw new Error('Second argument needs to be a Function');
+	}
+	if (!targetArray.length) { return true; }
+	for (let i=0; i<targetArray.length; i++) {
+		let result = fn(targetArray[i], i, targetArray);
+		if (!result) { return false; }
+	}
+	return true;
+}
 
-export { at, concat, entries };
+export { at, concat, entries, every };
